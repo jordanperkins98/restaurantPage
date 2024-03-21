d99b740f6903fce800f63e69eeb780e1baf276c8
Jordan Perkins <jordanperkins@hotmail.co.uk>
2024-03-21 21:11:29 +0000

    Added menu.js and updated index.js to allow tabbed pages

--- src/homepage.js
+++ src/homepage.js
@@ -1,36 +1,10 @@
 import myImagePath from './assets/_Burger-and-Beer.jpg';
 import secondImagePath from './assets/pexels-roman-odintsov-4552173.jpg';
 import myIcon from './assets/map-marker-radius.png';
+
 import './style.css';
+import {addImageToDiv, createDivWithClass, createImageWithSrc, createParagraphWithClass} from "./htmlmaker";
 
-
-// Function to create a div with a class
-function createDivWithClass(className) {
-  const div = document.createElement('div');
-  div.classList.add(className);
-  return div;
-}
-
-// Function to create a paragraph with a class and text
-function createParagraphWithClass(className, text) {
-  const p = document.createElement('p');
-  p.classList.add(className);
-  p.innerHTML = text;
-  return p;
-}
-
-// Function to create an image with a source
-function createImageWithSrc(src) {
-  const img = new Image();
-  img.src = src;
-  return img;
-}
-
-// Function to add an image to a div
-function addImageToDiv(div, imagePath) {
-  const img = createImageWithSrc(imagePath);
-  div.appendChild(img);
-}
 
 // Main function to create the homepage
 function homepage() {
@@ -68,25 +42,4 @@ function homepage() {
 export default homepage;
 
 
-/* <img src="./assets/_Burger-and-Beer.jpg" />
-<div class="sub-hero">
-<p class="hero-text">
-  Good Times <span>with</span><br />
-  Great Friends
-</p>
-<p class="hero-subtext">Discover Your Taste</p>
-<div class="address-container">
-  <img src="./assets/map-marker-radius.svg"/>
-  <p class="address">123 Main Street<br>
-    Oakville, CA <br>92655
-  </p>
-</div>
 
-</div>
-
-</div>
-<div class="about">
-<p class="hero-about">
-Serving Great Food <br> since 99
-</p>
-<img src="./assets/pexels-roman-odintsov-4552173.jpg"/> */
--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import homepage from "./homepage";
+import menu from "./menu";
+
+const navItems = Array.from(document.querySelectorAll('.tabs > a'));
+
+navItems.forEach(item => {
+    item.addEventListener('click', (e) =>{
+        const itemWithActiveClass = document.querySelector('.active');
+        itemWithActiveClass.classList.remove("active");
+        e.srcElement.classList.add("active");
+        const content = document.getElementById('content');
+        content.innerHTML = '';
+        if (e.srcElement.id === 'home') {
+            console.log("loading homepage....")
+            homepage();
+        } else if (e.srcElement.id === 'menu') {
+            console.log("loading menu....")
+            menu();
+        }
+    })
+})
 
 
 
 homepage(); // This will call the homepage function when the page loads.
+
+
+
+
